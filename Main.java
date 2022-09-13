e36333d5068ca95a95fde21ff1b64376f78dd3df
Newt <minhnguyen120203@gmail.com>
2022-09-13 20:47:32 +0700

    123

---
+++ Main.java
@@ -0,0 +1,16 @@
+public class Main {
+    public static void main(String[] args) {
+        String name;
+        float height;
+        int age;
+        name = "Newt";
+        height = 1.7f;
+        age = 24;
+        System.out.print("Ten:");
+        System.out.println(name);
+        System.out.print("Chieu cao:");
+        System.out.println(height);
+        System.out.print("Tuoi:");
+        System.out.println(age);
+    }
+}
---
+++ Myclass.java
@@ -0,0 +1,9 @@
+public class Myclass {
+    public void myMethod(){
+        System.out.println("Hello");
+    }
+    public static void main(String[] args) {
+        Myclass myObj = new Myclass();
+        myObj.myMethod();
+    }
+}
